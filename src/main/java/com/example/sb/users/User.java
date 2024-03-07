d7d371acfd4b9a3d63b0c3364d5875b5c09e25c3
YONG <yonghyunk09@gmail.com>
2024-03-07 13:48:48 +0900

    day 3

--- src/main/java/com/example/sb/users/User.java
+++ src/main/java/com/example/sb/users/User.java
@@ -11,18 +11,12 @@ public class User {
 	private int isDeleted;
 	
 	public User() { }
-	
-	
-	
 	public User(String uid, String pwd, String uname, String email) {
 		this.uid = uid;
 		this.pwd = pwd;
 		this.uname = uname;
 		this.email = email;
 	}
-
-
-
 	public User(String uid, String pwd, String uname, String email, LocalDate regDate, int isDeleted) {
 		this.uid = uid;
 		this.pwd = pwd;
